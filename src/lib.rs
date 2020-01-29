35f27e4b9c2d3f4e49c7e5d7e1e2a4ee642130bc
Garett Cooper <garettcooper@ymail.com>
2020-01-28 22:41:53 -0500

    Removed unnecessary extern crates

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,8 @@
 extern crate bitflags;
 #[macro_use]
 extern crate simple_error;
-//extern crate gc_nes_emulator;
-extern crate structopt;
 #[macro_use]
 extern crate log;
-
-use crate::structopt::StructOpt;
 
 pub mod cartridge;
 pub mod nes;
