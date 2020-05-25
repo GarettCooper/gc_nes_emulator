b4ef3c5b2a964684710a18e808dfb8abc3358468
Garett Cooper <garettcooper@ymail.com>
2020-05-24 22:46:17 -0400

    Added README to gc_nes_core

---
+++ gc_nes_core/README.md
@@ -0,0 +1,27 @@
+# gc_nes_core
+
+gc_nes_core is, as the name would suggest, the core of my Ninendo Entertainment System emulator.
+It provides an interface for dependent crates to load and run NES ROMs, provide input, and extract
+rendered image data. Audio is currently unsupported.
+
+#### Using the Emulator
+```rust, ignore
+use gc_nes_core::cartridge::Cartridge;
+use gc_nes_core::nes::Nes;
+
+fn main() {
+    // Load a .nes file as a cartridge
+    let cartridge = Cartridge::load_from_file("/some/nes/rom.nes".as_ref()).expect("File read error");
+    // Create the NES with the cartridge loaded
+    let mut nes = Nes::new(cartridge);
+    // Run the NES until the next frame completes
+    let frame_buffer:&[u32; 61440] = nes.frame();
+    // Or run it cycle by cycle for a finer approach
+    nes.cycle();
+    // Provide input state:
+    nes.update_controller_one(Some(0b0001_0100));
+    nes.update_controller_two(None); // Disconnected controller
+}
+```
+
+Current version: 0.1.0
--- gc_nes_core/src/lib.rs
+++ gc_nes_core/src/lib.rs
@@ -1,3 +1,27 @@
+//! gc_nes_core is, as the name would suggest, the core of my Ninendo Entertainment System emulator.
+//! It provides an interface for dependent crates to load and run NES ROMs, provide input, and extract
+//! rendered image data. Audio is currently unsupported.
+//!
+//! ### Using the Emulator
+//! ```rust, ignore
+//! use gc_nes_core::cartridge::Cartridge;
+//! use gc_nes_core::nes::Nes;
+//!
+//! fn main() {
+//!     // Load a .nes file as a cartridge
+//!     let cartridge = Cartridge::load_from_file("/some/nes/rom.nes".as_ref()).expect("File read error");
+//!     // Create the NES with the cartridge loaded
+//!     let mut nes = Nes::new(cartridge);
+//!     // Run the NES until the next frame completes
+//!     let frame_buffer:&[u32; 61440] = nes.frame();
+//!     // Or run it cycle by cycle for a finer approach
+//!     nes.cycle();
+//!     // Provide input state:
+//!     nes.update_controller_one(Some(0b0001_0100));
+//!     nes.update_controller_two(None); // Disconnected controller
+//! }
+//! ```
+
 #![allow(clippy::needless_return)] // I prefer clarity of return
 #![allow(clippy::verbose_bit_mask)] // For consistency in bit mask usage
 
