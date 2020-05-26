b36ffbade72c72dc42e1a73528cc2105ae5a2894
Garett Cooper <garettcooper@ymail.com>
2020-05-25 20:11:57 -0400

    Added README to gc_nes_web

--- gc_nes_web/src/lib.rs
+++ gc_nes_web/src/lib.rs
@@ -1,3 +1,29 @@
+//! gc_nes_web wraps the public functions exposed by my gc_nes_core crate for use
+//! in the browser through Web Assembly - Javascript interop. It provides an interface
+//! to load and run NES ROMs, provide input, and extract rendered image data.
+//! Audio is currently unsupported.
+//!
+//! ### Using the NES Emulator with Javascript
+//! ```javascript
+//! // Import the package
+//! const wasm = await import ("gc_nes_web");
+//! // Create the NES object
+//! let nes = this.state.wasm.nes(romArrayOfBytes);
+//! // Run the emulator to the completion of the next frame and retrieve it
+//! let frame = nes.frame();
+//! // Or run just one cycle and get the frame separately
+//! nes.cycle();
+//! let frame = nes.get_screen();
+//! // Drawing to a Canvas
+//! let offscreenCanvas = new OffscreenCanvas(256, 240);
+//! let offscreenCanvasContext = offscreenCanvas.getContext("2d");
+//! let imageData = offscreenCanvasContext?.createImageData(256, 240);
+//! imageData.data.set(frame);
+//! offscreenCanvasContext.putImageData(imageData, 0, 0);
+//! // mainCanvasContext is the 2D context for the Canvas you actually want to draw to.
+//! mainCanvasContext.drawImage(offscreenCanvas, 0, 0);
+//! ```
+
 mod utils;
 
 use gc_nes_core::cartridge::Cartridge;
@@ -95,4 +121,10 @@ pub fn nes(rom: &[u8]) -> WebNes {
 /// Gets the screen dimensions of the NES
 pub fn get_screen_dimensions() -> usize {
     NES_SCREEN_DIMENSIONS
+}
+
+#[wasm_bindgen]
+/// Enables the panic hook that logs panic messages to the browser console
+pub fn set_panic_hook() {
+    utils::set_panic_hook()
 }
